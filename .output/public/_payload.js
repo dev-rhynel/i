51d8ec71dfa7c74266baa6168724bcb36cbe2310
Rhynel Algopera <rhynel.developer@gmail.com>
2024-01-27 04:07:31 +0800

    Redeploy

--- .output/public/_payload.js
+++ .output/public/_payload.js
@@ -1,1 +1,1 @@
-export default {data:{},prerenderedAt:1706298653555}
+export default {data:{},prerenderedAt:1706299637786}
--- .output/public/about/_payload.js
+++ .output/public/about/_payload.js
@@ -1,1 +1,1 @@
-export default {data:{},prerenderedAt:1706298653535}
+export default {data:{},prerenderedAt:1706299637759}
